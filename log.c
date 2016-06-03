4d4a9114ee241158762c27b36550f97ca1363bec
wuhongtao_company <vi_pzp@163.com>
2016-06-03 15:50:22 +0800

    为几个函数添加了注释

--- log.c
+++ log.c
@@ -22,7 +22,12 @@ typedef struct log
 }LOG;
 
 static LOG s_log = {0};
-
+/*作者:吴洪涛
+ *功能:初始化日志模块
+ *参数:两个联合体，定义在log.h中
+ *参数说明:level是日志的预设级别，低于level级别的日志不会被输出，outWay是输出方式目前支持文件输出和控制台输出
+ *返回值:SECCESS表示成功，其他值表示失败，具体原因参照.h中的返回值宏
+ */
 int InitLog(LEVEL level, OUTWAY outWay)
 {
 	char logFileName[MAX_PATH] = {0};
@@ -65,7 +70,14 @@ int InitLog(LEVEL level, OUTWAY outWay)
 	return ret;
 }
 
-
+/*作者:吴洪涛
+ *功能:打印日志
+ *参数:将输出的日志的级别和其他的格式化的信息输入(参照printf使用)
+ *返回值:返回SECCESS表示成功，其他值表示失败，失败原因参考.h中返回值的宏
+ *注意:格式化输入需要注意格式的对应，如%s对应字符串，%d对应int型数值，由于只在程序内部使用，
+ *出于效率考虑，本函数并没有对参数进行复杂的检查，请使用者自己注意
+ *其他:为了增强代码可读性，.h中提供了一个宏PRINTLOG来代替本函数，请尽量使用宏代替本函数
+ */
 int PrintLog(LEVEL level, char *format, ...)
 {
 	va_list args;
@@ -119,6 +131,10 @@ int PrintLog(LEVEL level, char *format, ...)
 
 	return ret;
 }
+
+/*作者:吴洪涛
+ *功能:释放日志模块的资源
+ */
 void FreeLog()
 {
 	if (0 == s_log.initialized)
